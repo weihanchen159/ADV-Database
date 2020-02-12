84739f25d8ae6540b66e8d223bbe78843703203b
Jefflin <jefflin413@gmail.com>
2020-02-12 17:26:05 -0500

    create user interface

--- IR.py
+++ IR.py
@@ -12,39 +12,14 @@ import collections
 from sklearn.feature_extraction.text import TfidfVectorizer
 #nltk.download()
 
-# Retrieve top-10 Relevant Results From Google
-service = build("customsearch", "v1", developerKey="AIzaSyBKlE390a0krI3_Oe-zk-_Pdf7J0J7Oo1I")
-    
-res = service.cse().list(q='lectures', cx='000568897140034501999:kyspcqzrszq',).execute()
+import numpy as np
 
-URL = []
-for i in range(len(res['items'])):
-    URL.append(res['items'][i]['link'])
+import sys
 
-# Retrieve website context
-TEXT = []
-for i in range(len(URL)):
-    req = urllib.request.Request(URL[i], headers={'User-Agent': 'Mozilla/5.0'})
-    #html = urllib.request.urlopen(url).read()
-    try:
-        html = urllib.request.urlopen(req).read()
-        soup = BeautifulSoup(html)
-        # kill all script and style elements
-        for script in soup(["script", "style"]):
-            script.extract()    # rip it out
-
-        # get text
-        text = soup.get_text()
-    
-        # break into lines and remove leading and trailing space on each
-        lines = (line.strip() for line in text.splitlines())
-        # break multi-headlines into a line each
-        chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-        # drop blank lines
-        text = '\n'.join(chunk for chunk in chunks if chunk)
-        TEXT.append(text)
-    except HTTPError:
-        TEXT.append(res['items'][i]['snippet'])
+# $ python IR.py query precision
+query = sys.argv[1].split()
+origin_q = query
+precision = float(sys.argv[2])
 
 # Tokenize, take the stem of the words, remove stop words
 def get_tokens(text):
@@ -61,13 +36,104 @@ def stem_tokens(tokens, stemmer):
         stemmed.append(stemmer.stem(item))
     return stemmed
 
-TOKENS = [get_tokens(text) for text in TEXT]
-FILTERED = [[w for w in tokens if not w in stopwords.words('english')] for tokens in TOKENS]
-stemmer = PorterStemmer()
-STEMMED = [stem_tokens(filtered, stemmer) for filtered in FILTERED]
-corpus = [' '.join(stemmed) for stemmed in STEMMED]
+while True:
+    # Retrieve top-10 Relevant Results From Google
+    service = build("customsearch", "v1", developerKey="AIzaSyBKlE390a0krI3_Oe-zk-_Pdf7J0J7Oo1I")   
+    res = service.cse().list(q=' '.join(query), cx='000568897140034501999:kyspcqzrszq',).execute()
 
-vectorizer = TfidfVectorizer(min_df=1)
-vectorizer.fit_transform(corpus)
-feature_name = vectorizer.get_feature_names()
-tfidf = vectorizer.fit_transform(corpus).toarray()
+    # count the number of relevant results
+    cnt = 0
+    # store relevant results, (and its index in the retreival list, type: [(url1, index1), (url2, index2), ...])
+    URL = []
+
+    for i in range(len(res['items'])):
+        print(i+1, 'Title: ', res['items'][i]['title'], '\n')
+        print('URL: ', res['items'][i]['link'], '\n')
+        print('Description: ', res['items'][i]['snippet'], '\n')
+        print('\n\n')
+        reply = input("Relevant (Y/N)?")
+        if reply == 'Y':
+            URL.append((res['items'][i]['link'], i))
+            cnt += 1
+
+    if cnt >= precision * 10:
+        print('FEEDBACK SUMMARY\n')
+        print('Query: ', origin_q, '\n')
+        print('Precision: ', cnt/10, '\n')
+        print('Desired precision reached, done\n')
+        break
+    elif cnt == 0:
+        print('FEEDBACK SUMMARY\n')
+        print('Query: ', origin_q, '\n')
+        print('Precision: ', cnt/10, '\n')
+        print('No relevant result, terminate\n')
+        break
+    else:
+        print('FEEDBACK SUMMARY\n')
+        print('Query: ', origin_q, '\n')
+        print('Current Query: ', query, '\n')
+        print('Precision: ', cnt/10, '\n')
+        print('Not yet reach desired precision, continue\n')
+
+    # Retrieve website context
+    TEXT = []
+    for i in range(len(URL)):
+        req = urllib.request.Request(URL[i][0], headers={'User-Agent': 'Mozilla/5.0'})
+        # If the access being denied, append the short description to TEXT
+        try:
+            html = urllib.request.urlopen(req).read()
+            soup = BeautifulSoup(html, features="html.parser")
+            # kill all script and style elements
+            for script in soup(["script", "style"]):
+                script.extract()    # rip it out
+
+            # get text
+            text = soup.get_text()
+        
+            # break into lines and remove leading and trailing space on each
+            lines = (line.strip() for line in text.splitlines())
+            # break multi-headlines into a line each
+            chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+            # drop blank lines
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+            TEXT.append(text)
+        except HTTPError:
+            TEXT.append(res['items'][URL[i][1]]['snippet'])
+
+    # Tokenize the context
+    TOKENS = [get_tokens(text) for text in TEXT]
+    FILTERED = [[w for w in tokens if not w in stopwords.words('english')] for tokens in TOKENS]
+    stemmer = PorterStemmer()
+    STEMMED = [stem_tokens(filtered, stemmer) for filtered in FILTERED]
+    corpus = [' '.join(stemmed) for stemmed in STEMMED]
+
+    # reserve original words, so that we can add unprocessed word back to the query
+    bag_of_words = []
+    for filtered in FILTERED:
+        bag_of_words.extend(filtered)
+    count = collections.Counter(bag_of_words)
+
+    # obtain tf-idf matrix, row = number of documents, column = number of tokens (words)
+    vectorizer = TfidfVectorizer(min_df=1)
+    vectorizer.fit_transform(corpus)
+    feature_name = vectorizer.get_feature_names()
+    tfidf = vectorizer.fit_transform(corpus).toarray()
+
+    # find candidate word by choosing the token with highest summation value over all the document
+    # if this token already exists in the query, take the next highest one
+    # from the origin word collection, choose the most frequent word related to this token
+    flatten = np.sum(tfidf, axis = 0)
+    found_new_word = False
+    while not found_new_word:
+        cand_idx = flatten.argmax()
+        cand_root = feature_name[cand_idx]
+        if any(q.startswith(cand_root) for q in query):
+            flatten = np.delete(flatten, cand_idx)
+            continue
+        for word, freq in count.most_common(len(count)):
+            if word.startswith(cand_root):
+                query.append(word)
+                break
+        found_new_word = True
+    
+
